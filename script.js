90097bcf416001ee505bdcff55ea982f9ddbfa4a
feci <ferenchegymegi3@gmail.com>
2023-10-06 19:47:27 +0200

    students

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-const arr = [1, 2, 3, 4, 5]
-
-function retutnDouble(number) {
-    return number *2
-}
-
-const returnDouble2 = number => number * 2
-
-for (let i = 0; i < arr.length; i++) {
-    console.log(retutnDouble(arr[i] *2))
-}
-/* 
-document logDouble(number) {
-    console.log(number *2)
-}
+const data = [
+{
+    name: "kovács jános",
+    age: 29,
+    subjects: [
+        subjectName: "maths",
+        subjectGrade: 5,
+        subject: 
+            
+        ]       
+    ]
     
-arr.forEach(function(element) {
-console.log(element * 2)
+data.forEach(e => {
+    console.log(e.name)
+    e.subjects.forEach(dataElement => console.log(`${e.subjectsName}: ${e.subjectGrade}`))
 })
- */
-
-arr.forEach(element => console.log(element * 2)) //inline meghivas
