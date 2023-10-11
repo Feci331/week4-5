3b578d8833c996b051670bd24a6ed9ccba92255e
feci <ferenchegymegi3@gmail.com>
2023-10-11 16:52:14 +0200

    builtin update

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const data = [
-{
-    name: "kovács jános",
-    age: 29,
-    subjects: [
-        subjectName: "maths",
-        subjectGrade: 5,
-        subject: 
-            
-        ]       
-    ]
-    
-data.forEach(e => {
-    console.log(e.name)
-    e.subjects.forEach(dataElement => console.log(`${e.subjectsName}: ${e.subjectGrade}`))
-})
+//const arr = ["egy", "ketto", "három", "négy"]
+
+//console.log(arr.includes("text"))
+
+//console.log(["one", "two", "three", "four"].includes("four"))
+
+/* const obj = {
+    key1: "value",
+    key2: 10,
+    key3: function() {
+        console.log("hello")
+    }
+}
+
+console.log(obj.key3()) */
+
+const obj = {
+    name: "Gipsz Jakab",
+    age: 27,
+    pets: ["Mirci", "Lajcsi", "Géza"]
+}
+
+for (const key of Object.keys(obj)) {
+    console.log(key, obj[key])
+}
